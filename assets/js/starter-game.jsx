440e017a66b9651d8056206b19c44dc0fa64d87a
Prajakta Rodrigues <prajaktarodrigues@gmail.com>
2019-09-27 21:32:02 -0400

    Initial commit

--- assets/js/starter-game.jsx
+++ assets/js/starter-game.jsx
@@ -1,47 +1,206 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import _ from 'lodash';
+import css from "../css/starter-game.css";
 
 export default function game_init(root) {
-  ReactDOM.render(<Starter />, root);
+  ReactDOM.render(<Starter/>, root);
 }
 
 class Starter extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { left: false };
+    let currentTileState = this.initGameTiles();
+    this.state = {
+      openTiles: [],
+      currentTileState: currentTileState,
+      waiting: false,
+      score: 0,
+      isGameOver: false
+    };
   }
 
-  swap(_ev) {
-    let state1 = _.assign({}, this.state, { left: !this.state.left });
-    this.setState(state1);
+  restartGame() {
+    let currentTileState = this.initGameTiles();
+    let stateCopy = {
+      openTiles: [],
+      currentTileState: currentTileState,
+      waiting: false,
+      score: 0,
+      isGameOver: false
+    };
+    this.updateState(stateCopy);
   }
 
-  hax(_ev) {
-    alert("hax!");
+  initGameTiles() {
+    let currentTileState = [];
+    let alphabets = [
+      [
+        'A', 'B', 'C', 'D'
+      ],
+      [
+        'E', 'F', 'G', 'H'
+      ],
+      [
+        'A', 'B', 'C', 'D'
+      ],
+      [
+        'E', 'F', 'G', 'H'
+      ]
+    ]
+    for (var i = 0; i < 4; i++) {
+      let k = [];
+      for (var j = 0; j < 4; j++) {
+        k.push({class: "tile", value: alphabets[i][j],
+          open: false, done: false});
+      }
+      currentTileState.push(k);
+    }
+    return currentTileState;
+  }
+
+  checkIfGameOver(currentTileState) {
+    for (var i = 0; i < currentTileState.length; i++) {
+      for (var j = 0; j < currentTileState[i].length; j++) {
+        if (currentTileState[i][j].done === false) {
+          console.log(i, j);
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  updateState(stateCopy) {
+    this.setState(stateCopy);
+    console.log("state", stateCopy);
+
+  }
+
+  openTile(event) {
+    let id = event.target.id;
+    let stateCopy = _.cloneDeep(this.state);
+    if (stateCopy.waiting == true) {
+      return;
+    }
+    let i = id.split(" ")[0];
+    let j = id.split(" ")[1];
+
+    let modcurrentTileState = stateCopy.currentTileState;
+    let openTiles = stateCopy.openTiles;
+
+    if (modcurrentTileState[i][j].done === true) {
+      return;
+    }
+
+    if (openTiles.length == 1) {
+      let one = openTiles[0];
+      let iOne = one.split(" ")[0];
+      let jOne = one.split(" ")[1];
+
+      if (modcurrentTileState[i][j].value ==
+        modcurrentTileState[iOne][jOne].value) {
+        modcurrentTileState[i][j].class = "done-tile"
+        modcurrentTileState[i][j].done = true;
+        modcurrentTileState[i][j].open = false;
+        modcurrentTileState[iOne][jOne].class = "done-tile"
+        modcurrentTileState[iOne][jOne].done = true;
+        modcurrentTileState[iOne][jOne].open = false;
+        stateCopy.openTiles = [];
+        stateCopy.score += 20
+
+        let gameover = this.checkIfGameOver(stateCopy.currentTileState);
+        if (gameover) {
+          stateCopy.isGameOver = true;
+        }
+        this.updateState(stateCopy);
+      } else {
+        stateCopy.waiting = true;
+        stateCopy.openTiles.push(id);
+        modcurrentTileState[i][j].class = "open-tile"
+        modcurrentTileState[i][j].open = true;
+        this.updateState(stateCopy);
+        var self = this;
+        window.setTimeout(() => {
+          this.closeUnmatchedTiles();
+        }, 3000);
+      }
+    } else {
+      stateCopy.openTiles.push(id);
+      modcurrentTileState[i][j].class = "open-tile"
+      modcurrentTileState[i][j].open = true;
+      this.updateState(stateCopy);
+    }
+  }
+
+  closeUnmatchedTiles() {
+    let stateCopy = _.cloneDeep(this.state);
+    let modcurrentTileState = stateCopy.currentTileState;
+    let openTiles = stateCopy.openTiles;
+    for (var k = 0; k < openTiles.length; k++) {
+      let i = openTiles[k].split(" ")[0];
+      let j = openTiles[k].split(" ")[1];
+      modcurrentTileState[i][j].class = "tile"
+      modcurrentTileState[i][j].open = false;
+    }
+    stateCopy.openTiles = [];
+    stateCopy.waiting = false;
+    this.updateState(stateCopy);
   }
 
   render() {
-    let button = <div className="column" onMouseMove={this.swap.bind(this)}>
-      <p><button onClick={this.hax.bind(this)}>Click Me</button></p>
-    </div>;
 
-    let blank = <div className="column">
-      <p>Nothing here.</p>
-    </div>;
+    let items = [];
+    let item = [];
 
-    if (this.state.left) {
-      return <div className="row">
-        {button}
-        {blank}
-      </div>;
+    for (var i = 0; i < 4; i++) {
+      item = [];
+      for (var j = 0; j < 4; j++) {
+        if (this.state.currentTileState[i][j].open) {
+          item.push(<div key={(j * 21) + i} className="column-10 padding">
+            <button key={"btnkey" + i + "" + j} id={i + " " + j}
+              className={this.state.currentTileState[i][j].class}
+              onClick={(e) => this.openTile(e)}>
+              {this.state.currentTileState[i][j].value}
+            </button>
+          </div>);
+        } else {
+          item.push(<div key={(j * 21) + i} className="column-10 padding">
+            <button key={"btnkey" + i + "" + j} id={i + " " + j}
+              className={this.state.currentTileState[i][j].class}
+              onClick={(e) => this.openTile(e)}></button>
+          </div>);
+        }
+
+      }
+      items.push(<div className="row" key={i}>
+        {item}
+      </div>);
     }
-    else {
-      return <div className="row">
-        {blank}
-        {button}
-      </div>;
+    if (!this.state.isGameOver) {
+      return (<div>
+        <h1>
+          Memory Test Game
+        </h1>
+        <h3>
+          <button className="replay"
+            onClick={this.restartGame.bind(this)}>Restart</button>
+        </h3>
+        <div>
+          {items}
+        </div>
+        <div>
+          <h2>Current Score: {this.state.score}</h2>
+        </div>
+      </div>)
+    } else {
+      return (<div>
+        <h1>
+          Game Over!! Your score is {this.state.score}.
+        </h1>
+        <button className="replay"
+          onClick={this.restartGame.bind(this)}>Play Again</button>
+      </div>)
     }
   }
 }
-
